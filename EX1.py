c05b4be1beb334cf06c304c95afb2e2020fda6fc
lebranchu mehdi <mehdi.lebranchu@gmail.com>
2020-03-24 11:19:02 +0100

    exo terminé

--- EX1.py
+++ EX1.py
@@ -4,5 +4,19 @@ class Cercle:
     def getR(self):
         return self.__r
     def __add__(self, other):
-        return self.__r + other.__r
-
+        return Cercle(self.__r + other.__r)
+    def __lt__(self,other):
+        return self.__r < other.__r
+    def __gt__(self, other):
+        return self.__r > other.__r
+    def __str__(self):
+        return str(self.__r)
+if __name__ == '__main__':
+    c = Cercle(2)
+    c2 = Cercle(3)
+    c3 = c + c2
+    c4 = c < c2
+    c5 =  c > c3
+    print(c3)
+    print(c4)
+    print(c5)
